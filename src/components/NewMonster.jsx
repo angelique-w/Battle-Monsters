7a7e52bb365e3cc6a4c9e40fdefa31152e49ecac
angelique-w <angelique.wons@gmail.com>
2019-10-30 16:52:38 +0100

    created NewMonster component

---
+++ src/components/NewMonster.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd';
+
+function NewMonster() {
+    return (
+        <div>
+             <Button size={size}>Normal</Button>
+        </div>
+    );
+}
+
+export default NewMonster;
