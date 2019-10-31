4bb08ea1dbf557a92aa55471ef93a88828516ca6
KleinosFR <kleinos@hotmail.com>
2019-10-31 09:25:57 +0100

    created display battle component

--- src/components/BattleDisplay.jsx
+++ src/components/BattleDisplay.jsx
@@ -1,13 +1,15 @@
 import React from "react"
-import {Container, Row} from "reactstrap"
+import { Container, Row } from "reactstrap"
 
-const BattleDisplay = ({message}) => {
+const BattleDisplay = ({ messages }) => {
 
 
-    return(
+    return (
         <Container>
-            <Row>
-        <p>{message}</p>
+            <Row className="justify-content-center">
+                {messages.length !== 0 ? (messages.map(msg => {
+                    return <p>{msg}</p>
+                })) : "Ready for Battle"}
             </Row>
         </Container>
     )
